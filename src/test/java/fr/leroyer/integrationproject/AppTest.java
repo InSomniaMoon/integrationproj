3ab86edc48cef6c310531a8ccd633032abfa6849
InSomniaMoon <pierre.leroyer69@gmail.com>
2022-05-30 16:35:01 +0200

    undo test

--- src/test/java/fr/leroyer/integrationproject/AppTest.java
+++ src/test/java/fr/leroyer/integrationproject/AppTest.java
@@ -15,10 +15,5 @@ public class AppTest {
     public void shouldAnswerWithTrue() {
         assertTrue(true);
     }
-
-    @Test
-    public void shouldFail() {
-        assertTrue("will fail", false);
-    }
 }
 
